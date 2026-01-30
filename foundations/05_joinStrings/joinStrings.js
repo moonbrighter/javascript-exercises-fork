a86521860444083af489019732cbc71c7d1ae216
Ricky Marc <rickymarcdev@gmail.com>
2026-01-30 19:28:36 +0800

    Finish foundations/05_joinStrings

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -1,19 +1,12 @@
-/*
- First name: Carlos
- Last name: Stevenson
- This year: 1965
- Birth year: 1947
+let firstName = "Carlos";
+let lastName = "Stevenson";
+let fullName = firstName + " " + lastName;
+let thisYear = 1965;
+let birthYear = 1947;
+let age = thisYear - birthYear;
 
- The greeting should read: "Hello! My name is Carlos Stevenson and I am 18 years old."
-*/
+greeting = "Hello! My name is " + fullName + " and I am " + age + " years old.";
 
-// Add your code right below, good luck!
-
-
-
-
-
-// Do not change this
 module.exports = {
   firstName: typeof firstName === 'undefined' ? undefined : firstName,
   lastName: typeof lastName === 'undefined' ? undefined : lastName,
